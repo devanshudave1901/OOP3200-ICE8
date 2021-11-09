10af10515211ffa37b5586e48c547baa8e034b18
Devanshu Dave <devanshudave1901@gmail.com>
2021-11-08 23:40:10 -0500

    Includes input and output functionality

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,28 @@
+/**
+ * @author Devanshu Dave
+ * @date 08/11/2021
+ * @type ICE8
+ */
+
+
 package ca.durhamcollege;
+
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        String name = null;
+        int age;
+
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please Enter your name ");
+        name = keyboard.nextLine(); // input inside the keyboard a string
+
+        System.out.printf("You entered: %s ", name);
+
+
     }
 }
