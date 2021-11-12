585c48a6a656c4348a8684f2335c4ab20373370c
Devanshu <devanshudave022@gmail.com>
2021-11-12 11:25:51 -0500

    added validation for user's age

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -7,6 +7,7 @@
 
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -25,14 +26,45 @@ public class Main {
 
 
         float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
-        System.out.println();
 
+        boolean isValidInput = false;
+
+        do
+        {
+            System.out.println("Please Enter Your age: ");
+            try
+            {
+
+                age = keyboard.nextFloat();
+                System.out.println();
+                isValidInput = true;
+                if((age  < 0.0f)  ||  (age > 120.0f))
+                {
+                    System.out.println("Error: You must enter a age greater than 0.0 and less than 120.0 years old");
+                    isValidInput = false;
+
+                }
+
+
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating-point number");
+                keyboard.nextLine();
+                isValidInput = false;
+
+
+            }
+        }
+        while(!isValidInput);
 
         //output
-        System.out.printf("You entered: %s ", name);
+        System.out.printf("\nYou entered: %s \n", name);
         System.out.printf("You Entered: %.2f \n",age);
+
+
+
+
 
     }
 }
